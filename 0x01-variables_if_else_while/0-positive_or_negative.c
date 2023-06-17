e0ee4d2cc259f13a5afeeb13a4125c3960fbd794
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-17 00:36:14 +0000

    Commited !

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,6 +17,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d\n", n);
+	if (n > 0)
+	{
+		printf("%d", n, "is positive\n");
+	} else
+	{
+		printf("%d", n, "is negative\n");
+	}
 	return (0);
 }
