d502ffbf3e36594ff00b8f70a2a66229b309bf7d
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-17 02:14:16 +0000

    Commited !

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program that prints number from 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 48;
+
+	while (ch <= 57)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n')
+	return (0);
+}
