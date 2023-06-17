e5f1de1cd070bcefeca46afb838e83b582bec3cc
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-17 02:30:29 +0000

    Commited !

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program that prints alphabet backwards
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 122;
+
+	while (ch >= 97)
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
