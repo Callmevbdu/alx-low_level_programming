d56a6a0b7e4308d531c709ff1afba82b31fd3926
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-17 00:26:15 +0000

    Commited !

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+/**
+ *	main - Entry point
+ *
+ *	Description: A C program that prints a n number.
+ *
+ *	return is 0 (success)
+ *
+ */
 
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	printf("%d\n", n;
 	return (0);
 }
