4412a9b1157a8552d6b7c1c5d042212867fe9188
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-17 02:59:32 +0000

    Commited !

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program that prints all possible combinations of two digits
+ * excludig the combination of the same digit ex "00", "11" ...
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int dig1 = 0, dig2;
+
+	while (dig1 <= 9)
+	{
+		dig2 = 0;
+		while (dig2 <= 9)
+			if (dig1 != dig2 && dig1 < dig2)
+			{
+				putchar(dig1 + 48);
+				putchar(dig2 + 48);
+
+				if (dig1 + dig2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			dig2++;
+		}
+		dig1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
