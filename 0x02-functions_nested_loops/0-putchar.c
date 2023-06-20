ebfd7f8f39adf5f4bf199b61d4e5921ec88cb98b
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-20 21:26:33 +0000

    Commited!

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A C program that prints "_putchar"
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
