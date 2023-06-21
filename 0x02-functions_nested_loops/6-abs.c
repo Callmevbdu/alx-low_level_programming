b59600bb49daec452d45e33fc7700e0940d45ae3
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-21 02:40:02 +0000

    Commited!

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - a prototype that computes the absolute value of an integer
+ * @n: an integer
+ * Return 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
