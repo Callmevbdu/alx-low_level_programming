14f8d1c7311b5a34b1cb0d6a30c276b0d8d13408
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-21 04:16:20 +0000

    Commited!

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers
+ * @a: first input
+ * @b: second input
+ * Return: sum of the addition
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
