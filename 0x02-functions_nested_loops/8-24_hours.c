948435d5afdbd3b59525c63a6194c60b5de3451d
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-21 04:29:06 +0000

    Commited!

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,14 +6,14 @@
 
 void jack_bauer(void)
 {
-	int min, hr;
+	int min, hour;
 
-	for (hr = 0; min <= 59; min++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar((hr / 10) + 48);
-			_putchar((hr % 10) + 48);
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
 			_putchar(':');
 			_putchar((min / 10) + 48);
 			_putchar((min % 10) + 48);
