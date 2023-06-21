13351dd49583b88bb798c5507e8b72804dd434c4
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-21 01:06:50 +0000

    Commited!

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point
- * Description: A C program that prints "_putchar"
+ * Description: A C program that prints "_putchar" using a prototype.
  * Return: 0 (success)
 */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char string[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(string[ch]);
+	_putchar('\n');
 
 	return (0);
 }
