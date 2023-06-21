c4c0600413dc166c758b9d0516a1d4412bece958
“Abdellatif” <abdelt.2012@gmail.com>
2023-06-21 03:13:06 +0000

    Commited!

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - a function that prints every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; min <= 59; min++)
+	{
+		for (min 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+
+		}
+
+	}
+
+}
