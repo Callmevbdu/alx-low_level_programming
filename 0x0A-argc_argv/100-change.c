ea946e48e1b3afb9862dbbb3a6b100b589e520be
Abdellatif <abdelt.2012@gmail.com>
2023-07-09 01:26:05 +0000

    Task done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,22 +13,22 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int i, lm = 0, m = atoi(argv[1]);
-		int c[] = {25, 10, 5, 2, 1};
+	int i, lm = 0, m = atoi(argv[1]);
+	int c[] = {25, 10, 5, 2, 1};
 
-		for (i = 0; i < 5; i++)
+	for (i = 0; i < 5; i++)
+	{
+		if (m >= c[i])
 		{
-			if (m >= c[i])
+			lm += m / c[i];
+			m = m % c[i];
+			if (m % c[i] == 0)
 			{
-				lm += m / c[i];
-				m = m % c[i];
-				if (m % c[i] == 0)
-				{
-					break;
-				}
+				break;
 			}
 		}
-		printf("%d\n", lm);
+	}
+	printf("%d\n", lm);
 	}
 	else
 	{
