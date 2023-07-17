e6bf943d5f40f71f616d35e8433ab055021c5e81
Abdellatif <abdelt.2012@gmail.com>
2023-07-17 23:59:19 +0000

    Task done.

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+#include <stdio.h>
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+int _putchar(char c);
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,9 @@
+#ifndef _SUM_H
+#define _SUM_H
+
+#include <stdio.h>
+#define SUM(x, y) ((x) + (y))
+
+int _putchar(char c);
+
+#endif
