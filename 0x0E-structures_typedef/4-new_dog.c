dc0a3fb5ac0c2eb6c78b14e255cf9c5d714dee33
Abdellatif <abdelt.2012@gmail.com>
2023-07-18 03:09:10 +0000

    Task done.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,48 +3,39 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
+ * @s: string
+ * Return: length of string
  */
+
 int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
-
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 
 	return (i);
 }
 
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
+ * _strcpy - copies the string
+ * @dest: pointer to the buffer
  * @src: string to be copied
- *
- * Return: the pointer to dest
+ * Return: pointer to dest
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
 	len = 0;
-
 	while (src[len] != '\0')
-	{
 		len++;
-	}
 
 	for (i = 0; i < len; i++)
-	{
 		dest[i] = src[i];
-	}
+
 	dest[i] = '\0';
 
 	return (dest);
@@ -55,8 +46,7 @@ char *_strcpy(char *dest, char *src)
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * Return: pointer to the new dog (Success), "NULL" otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -89,4 +79,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (dog);
 }
-
