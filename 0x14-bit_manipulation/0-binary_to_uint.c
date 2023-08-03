17c190ce44da528ea3235c773da7e8107f75ccce
Abdellatif <abdelt.2012@gmail.com>
2023-08-03 22:12:03 +0000

    Task done.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned
+ * intfunction that converts a binary number to an unsigned int
+ * @b: input binary number
+ * Return: The converted number, otherwise "1"
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		n = n * 2 + (*b++ - '0');
+	}
+	return (n);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This is my readme file for the "0x14-bit_manipulation" project.
