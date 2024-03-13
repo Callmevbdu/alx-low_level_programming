0522944c3ef48aa865c37cccea50c5049bc86f83
Abdellatif <abdelt.2012@gmail.com>
2024-03-13 22:12:58 +0000

    Done.

--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -8,17 +8,9 @@
 int main(void)
 {
 	int array[] = {
-		10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+		0, 1, 2, 8, 13, 17, 19, 20, 21, 22, 24, 50, 100
 	};
 	size_t size = sizeof(array) / sizeof(array[0]);
-
-	/* Test cases for linear search */
-	printf("Found %d at index: %d\n\n", 8, linear_search(array, size, 8));
-	printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-	printf("Found %d at index: %d\n\n", 0, linear_search(array, size, 0));
-	printf("Found %d at index: %d\n\n", -1, linear_search(array, size, -1));
-	printf("Found %d at index: %d\n\n", 99, linear_search(array, size, 99));
-
 	/* Test cases for binary search */
 	printf("Found %d at index: %d\n\n", 8, binary_search(array, size, 8));
 	printf("Found %d at index: %d\n\n", 100, binary_search(array, size, 100));
